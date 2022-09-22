42811b114708066e680693ad157c827796bd41bc
abr-ya <gamer05@list.ru>
2022-09-22 08:58:22 +0300

    fix: Add clg

--- src/services/gapi.ts
+++ src/services/gapi.ts
@@ -1,9 +1,11 @@
 import { gapi } from "gapi-script";
 
 export const gapiInit = () => {
+  console.log("gapi init");
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+  const scope = "email";
+  console.log("init:", clientId.slice(0, 20), scope);
   function start() {
-    const clientId = process.env.GOOGLE_CLIENT_ID;
-    const scope = "email";
     console.log("start:", clientId.slice(0, 20), scope);
     gapi.auth2.init({ clientId, scope });
   }
