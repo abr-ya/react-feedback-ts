c1e020e95d006c03cc74fe9ed9c67e1283705d83
abr-ya <gamer05@list.ru>
2022-09-23 07:55:56 +0300

    fix: Update env-creator

--- nenv.js
+++ nenv.js
@@ -1,4 +1,9 @@
 const fs = require("fs");
 // здесь необходимо перечислить ключи из файла ENV, а точнее - из ENV netlify
-fs.writeFileSync("./.env", `GOOGLE_CLIENT_ID=${process.env.GOOGLE_CLIENT_ID}\n`);
-fs.writeFileSync("./.env", `API_URL=${process.env.API_URL}\n`);
+fs.writeFileSync(
+  "./.env",
+  `
+    GOOGLE_CLIENT_ID=${process.env.GOOGLE_CLIENT_ID}\n
+    API_URL=${process.env.API_URL}\n
+  `,
+);
